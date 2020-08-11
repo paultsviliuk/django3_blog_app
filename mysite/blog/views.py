a7b6a6d16b8a96209e852f3ca84314e35401e5b8
angmar33 <paultsvilyuk@gmail.com>
2020-08-11 22:58:37 +0300

    class based view added. templates updated. related imports removed.static files added.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.views.generic import ListView
+from django.core.paginator import Paginator, EmptyPage,\
+                                     PageNotAnInteger
+
+from blog.models import Post
 
 
-def post_list(request):
-    posts = Post.published.all()
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+"""def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'page': page,
+                   'posts': posts})"""
 
 
 def post_detail(request, year, month, day, post):
