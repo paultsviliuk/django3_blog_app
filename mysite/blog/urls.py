ed1bd8005c103a70b079fda15088380c2cc830bc
angmar33 <paultsvilyuk@gmail.com>
2020-08-14 23:24:53 +0300

    share post via email api added.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -7,8 +7,13 @@ app_name = 'blog'
 
 urlpatterns = [
     # post views
-    path('', views.PostListView.as_view(), name='post_list'),
+    path('',
+         views.PostListView.as_view(),
+         name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail,
          name='post_detail'),
+    path('<int:post_id>/share/',
+         views.post_share,
+         name='post_share'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage,\
                                      PageNotAnInteger
+from django.core.mail import send_mail
 
 from blog.models import Post
+from blog.forms import EmailPostForm
 
 
 class PostListView(ListView):
@@ -11,6 +13,33 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'admin@myblog.com',
+                      [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
 
 
 """def post_list(request):
