5fd3c7156dcb10fef970b28f2861af7988483f36
angmar33 <paultsvilyuk@gmail.com>
2020-08-18 21:33:55 +0300

    requirements added. tagging added to posts.

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+from taggit.managers import TaggableManager
+
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -34,6 +36,7 @@ class Post(models.Model):
 
     objects = models.Manager()  # The default manager.
     published = PublishedManager()  # Our custom manager.
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage,\
                                      PageNotAnInteger
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 from blog.models import Post, Comment
 from blog.forms import EmailPostForm, CommentForm
@@ -42,8 +43,14 @@ def post_share(request, post_id):
                                                     'sent': sent})
 
 
-"""def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3)  # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -57,7 +64,8 @@ def post_share(request, post_id):
     return render(request,
                   'blog/post/list.html',
                   {'page': page,
-                   'posts': posts})"""
+                   'posts': posts,
+                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
