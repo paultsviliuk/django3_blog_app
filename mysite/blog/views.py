3514bd92ae029e242e8de28254ce45f6d8dd7190
angmar33 <paultsvilyuk@gmail.com>
2020-08-18 22:24:14 +0300

    Retrieving posts by similarity functionality added.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage,\
@@ -91,9 +92,16 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids
+                                          ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')
+                                           ).order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
                    'new_comment': new_comment,
-                   'comment_form': comment_form})
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
