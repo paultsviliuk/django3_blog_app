59e9c74c42f9087de1edf34650baff97d14280c1
angmar33 <paultsvilyuk@gmail.com>
2020-08-19 21:12:51 +0300

    sitemap added.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
+
 
 # Application definition
 
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'blog',
     'taggit',
 ]
