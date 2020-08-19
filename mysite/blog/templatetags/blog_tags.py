c315674f3df5ecc9d719fc9305894fce00d9958e
angmar33 <paultsvilyuk@gmail.com>
2020-08-19 20:24:16 +0300

    custom filter added for templates.

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,5 +1,7 @@
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 from blog.models import Post
 
@@ -23,3 +25,8 @@ def get_most_commented_posts(count=5):
     return Post.published.annotate(
         total_comments=Count('comments')
     ).order_by('-total_comments')[:count]
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
